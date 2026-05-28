08cfc19919b201698f5f72cfaa17bbf2321736eb
jamesmeya572-lang <jamesmeya572@gmail.com>
2026-05-28 20:19:29 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,9 @@
-print("Bienvenue dans CryoExpert")
-print("Application de Froid Industriel")
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def accueil():
+    return "Bienvenue dans CryoExpert !"
+
+if __name__ == "__main__":
+    app.run()
