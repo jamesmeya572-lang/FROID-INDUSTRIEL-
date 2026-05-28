838e94972265ffc859384051d6031fbec4d09205
jamesmeya572-lang <jamesmeya572@gmail.com>
2026-05-28 21:04:06 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -6,4 +8,5 @@ def accueil():
     return "Bienvenue dans CryoExpert !"
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
